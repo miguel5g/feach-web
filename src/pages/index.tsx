bf9cd031cedcb8a62fe1bf687f96e613810a33be
Miguel Ângelo <miguelcg958@gmail.com>
2021-01-09 23:23:58 -0300

    Adicionado alguns itens na página index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
+import { FaDiscord } from 'react-icons/fa';
 
 import Header from '../components/Header';
 
-import { Container } from '../styles/pages/Index';
+import {
+  S1, S2, Container, PageDesc, PageTitle, StyledLink, S1Image, S2Title, S2GameList, S2GameItem, S2GameTitle, S2GameIcon, S2GamePrice,
+} from '../styles/pages/Index';
 
 const Home: React.FC = () => (
   <>
@@ -13,7 +17,75 @@ const Home: React.FC = () => (
 
     <Header />
 
-    <Container />
+    <Container>
+      <S1>
+        <div>
+          <PageTitle>Feach Comunity</PageTitle>
+          <PageDesc>
+            Somos uma comunidade recem nascida com o
+            propósito de juntar varias informações em um
+            só lugar, desde de juntar pessas para jogarem
+            até sobre os melhores preços para comprar seus jogos
+          </PageDesc>
+
+          <Link href="/about" passHref>
+            <StyledLink primary>
+              <FaDiscord />
+              Comunidade
+            </StyledLink>
+          </Link>
+        </div>
+
+        <S1Image src="/img/not-found.png" />
+      </S1>
+
+      <S2>
+        <S2Title>Jogos com descontos</S2Title>
+
+        <S2GameList>
+          <S2GameItem>
+            <S2GameIcon src="/svg/no-data.svg" />
+            <S2GameTitle>Minecraft</S2GameTitle>
+            <S2GamePrice>
+              R$30,50
+              <span>R$130,50</span>
+            </S2GamePrice>
+          </S2GameItem>
+          <S2GameItem>
+            <S2GameIcon src="/svg/no-data.svg" />
+            <S2GameTitle>Minecraft</S2GameTitle>
+            <S2GamePrice>
+              R$30,50
+              <span>R$130,50</span>
+            </S2GamePrice>
+          </S2GameItem>
+          <S2GameItem>
+            <S2GameIcon src="/svg/no-data.svg" />
+            <S2GameTitle>Minecraft</S2GameTitle>
+            <S2GamePrice>
+              R$30,50
+              <span>R$130,50</span>
+            </S2GamePrice>
+          </S2GameItem>
+          <S2GameItem>
+            <S2GameIcon src="/svg/no-data.svg" />
+            <S2GameTitle>Minecraft</S2GameTitle>
+            <S2GamePrice>
+              R$30,50
+              <span>R$130,50</span>
+            </S2GamePrice>
+          </S2GameItem>
+          <S2GameItem>
+            <S2GameIcon src="/svg/no-data.svg" />
+            <S2GameTitle>Minecraft</S2GameTitle>
+            <S2GamePrice>
+              R$30,50
+              <span>R$130,50</span>
+            </S2GamePrice>
+          </S2GameItem>
+        </S2GameList>
+      </S2>
+    </Container>
   </>
 );
 
--- src/styles/pages/Index.ts
+++ src/styles/pages/Index.ts
@@ -1,8 +1,128 @@
 import styled from 'styled-components';
+import BaseLink from '../../components/BaseLink';
 
-// eslint-disable-next-line import/prefer-default-export
 export const Container = styled.main`
   height: max-content;
 
   padding-bottom: 25px;
 `;
+
+const Section = styled.section`
+  display: flex;
+
+  margin: 25px 60px;
+`;
+
+/* Section 1 */
+
+export const S1 = styled(Section)`
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0 60px;
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    max-width: 42%;
+  }
+`;
+
+export const PageTitle = styled.h1`
+  margin-bottom: 7px;
+
+  color: ${({ theme }) => theme.txt.primary};
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 2.6rem;
+`;
+
+export const PageDesc = styled.p`
+  margin-bottom: 23px;
+
+  font-size: 1.3rem;
+`;
+
+export const S1Image = styled.img`
+  width: 42%;
+`;
+
+export const StyledLink = styled(BaseLink)`
+`;
+
+/* Section 2 */
+
+export const S2 = styled(Section)`
+  flex-direction: column;  
+
+  margin-top: 64px;
+  padding: 0 60px;
+`;
+
+export const S2Title = styled.h3`
+  margin-bottom: 13px;
+
+  color: ${({ theme }) => theme.txt.primary};
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 1.9rem;
+  font-weight: normal;
+`;
+
+export const S2GameList = styled.ul`
+  display: flex;
+
+  list-style: none;
+`;
+
+export const S2GameItem = styled.li`
+  display: flex;
+  flex-direction: column;
+
+  width: 184px;
+  margin-right: 15px;
+
+  border-radius: 13px;
+  background-color: ${({ theme }) => theme.bg.primary};
+
+  overflow: hidden;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+export const S2GameTitle = styled.h4`
+  margin: 0 13px;
+  margin-bottom: 5px;
+
+  color: ${({ theme }) => theme.txt.primary};
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
+
+export const S2GameIcon = styled.img`
+  width: 100%;
+  margin-bottom: 13px;
+
+  background-color: ${({ theme }) => theme.bg.tertiary};
+`;
+
+export const S2GamePrice = styled.span`
+  margin: 13px;
+
+  color: ${({ theme }) => theme.txt.primary};
+  font-size: 1.2rem;
+
+  & > span {
+    margin-left: 3px;
+
+    color: ${({ theme }) => theme.txt.secondary};
+    font-size: 1rem;
+    font-style: italic;
+    text-decoration: line-through;
+  }
+`;
+
+// export const GameList = styled.div``;
