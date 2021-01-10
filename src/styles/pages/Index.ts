43a42a0b1c56f2aca1b4d5549ed749bd8dbd95fc
Miguel Ângelo <miguelcg958@gmail.com>
2021-01-10 11:56:14 -0300

    Adicionado responsividade para página Index

--- src/styles/pages/Index.ts
+++ src/styles/pages/Index.ts
@@ -11,13 +11,17 @@ const Section = styled.section`
   display: flex;
 
   margin: 25px 60px;
+
+  @media (max-width: 826px) {
+    margin: 25px;
+  }
 `;
 
 /* Section 1 */
 
 export const S1 = styled(Section)`
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
 
   padding: 0 60px;
 
@@ -26,7 +30,29 @@ export const S1 = styled(Section)`
     flex-direction: column;
     align-items: flex-start;
 
-    max-width: 42%;
+    max-width: 600px;
+    margin-right: 60px;
+  }
+
+  @media (max-width: 1600px) {
+    justify-content: space-between;
+  }
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    padding: 0;
+
+    & > div {
+      text-align: center;
+      justify-content: center;
+      align-items: center;
+
+      margin-right: 0;
+      margin-bottom: 23px;
+    }
   }
 `;
 
@@ -46,6 +72,8 @@ export const PageDesc = styled.p`
 
 export const S1Image = styled.img`
   width: 42%;
+  max-width: 462px;
+  min-width: 270px;
 `;
 
 export const StyledLink = styled(BaseLink)`
@@ -54,10 +82,15 @@ export const StyledLink = styled(BaseLink)`
 /* Section 2 */
 
 export const S2 = styled(Section)`
-  flex-direction: column;  
+  flex-direction: column;
+  align-items: center;
 
   margin-top: 64px;
   padding: 0 60px;
+
+  @media (max-width: 768px) {
+    padding: 0;
+  }
 `;
 
 export const S2Title = styled.h3`
@@ -71,6 +104,8 @@ export const S2Title = styled.h3`
 
 export const S2GameList = styled.ul`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 
   list-style: none;
 `;
@@ -80,6 +115,7 @@ export const S2GameItem = styled.li`
   flex-direction: column;
 
   width: 184px;
+  margin: 15px 0;
   margin-right: 15px;
 
   border-radius: 13px;
