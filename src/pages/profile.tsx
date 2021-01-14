2dc919abf49f339b154712048b89dd4d2eab3728
Miguel Ângelo <miguelcg958@gmail.com>
2021-01-14 14:51:04 -0300

    Adicionado página de usuário

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,17 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { FiLock, FiLogIn, FiUser } from 'react-icons/fi';
+import {
+  FiArrowLeft,
+  FiCheckCircle,
+  FiClock, FiHash, FiLink, FiLogIn, FiMail, FiUser,
+} from 'react-icons/fi';
+import { TiBusinessCard } from 'react-icons/ti';
+import { BsInfoCircle } from 'react-icons/bs';
 
+import { FaDiscord } from 'react-icons/fa';
 import api from '../services/Api';
 
 import {
-  Container,
+  Container, User, UserInfo, UserInfos, UserHeader, UserName, UserAvatar, LinkButton, BackButton,
 } from '../styles/pages/Profile';
+
+interface IUserData {
+  id: string;
+  user_number: number;
+  actived: boolean;
+  username: string;
+  email: string;
+  password: null;
+  permission: '0' | '1' | '2';
+  discord_id: string | null;
+  avatar_url: string | null;
+  created_at: string;
+  updated_at: string;
+}
+
+const permissionResolver: {[key: string]: string} = {
+  0: 'Usuário',
+  1: 'Editor',
+  2: 'Admin',
+};
 
 const Login: React.FC = () => {
   const router = useRouter();
+  const [user, setUser] = useState<IUserData>();
+
+  useEffect(() => {
+    api.get<IUserData>('/user', { withCredentials: true })
+      .then(({ data }) => {
+        setUser(data);
+      })
+      .catch(() => {
+        router.replace('/login');
+      });
+  }, []);
 
   return (
     <>
@@ -19,7 +56,95 @@ const Login: React.FC = () => {
         <title>Meu perfil</title>
       </Head>
 
-      <Container />
+      <Container>
+        {user && (
+          <User>
+            <UserHeader>
+              <UserAvatar>
+                <img src={user.avatar_url ? user.avatar_url : '/svg/default_user.svg'} alt="User avatar" />
+              </UserAvatar>
+              <UserName>
+                {user.username}
+                <span>
+                  {user.id}
+                  {' '}
+                  #
+                  {user.user_number}
+                </span>
+              </UserName>
+            </UserHeader>
+
+            <UserInfos>
+              <UserInfo>
+                <TiBusinessCard />
+                ID
+                <span>{user.id}</span>
+              </UserInfo>
+
+              <UserInfo>
+                <FiLogIn />
+                Número de registro
+                <span>
+                  <FiHash />
+                  {user.user_number}
+                </span>
+              </UserInfo>
+
+              <UserInfo>
+                <FiCheckCircle />
+                Verificada
+                <span>{user.actived ? 'Sim' : 'Não'}</span>
+              </UserInfo>
+
+              <UserInfo>
+                <FiUser />
+                Nome de usuário
+                <span>{user.username}</span>
+              </UserInfo>
+
+              <UserInfo>
+                <FiMail />
+                Email
+                <span>{user.email}</span>
+              </UserInfo>
+
+              <UserInfo>
+                <BsInfoCircle />
+                Nível de permissão
+                <span>{permissionResolver[user.permission]}</span>
+              </UserInfo>
+
+              <UserInfo>
+                <FaDiscord />
+                ID do Discord
+                <span>{user.discord_id ? user.discord_id : 'Não vinculado' }</span>
+              </UserInfo>
+
+              <UserInfo>
+                <FiClock />
+                Criada em
+                <span>{user.created_at}</span>
+              </UserInfo>
+            </UserInfos>
+
+            {user.discord_id && (
+              <LinkButton primary>
+                <FiLink />
+                Vincular Discord
+              </LinkButton>
+            )}
+          </User>
+        )}
+
+        <BackButton
+          onClick={() => router.back()}
+        >
+          <FiArrowLeft />
+          Voltar
+        </BackButton>
+
+      </Container>
+
     </>
   );
 };
--- src/styles/pages/Profile.ts
+++ src/styles/pages/Profile.ts
@@ -1,6 +1,177 @@
 import styled from 'styled-components';
+import Button from '../../components/Button';
 
-// eslint-disable-next-line import/prefer-default-export
 export const Container = styled.main`
-  
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 60px 30px;
+`;
+
+export const User = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  top: 0;
+  left: 0;
+  width: 65vw;
+  min-width: 660px;
+  padding: 30px;
+
+  background: ${({ theme }) => theme.bg.primary};
+  border-radius: 5px;
+
+  overflow: hidden;
+
+  /* &::after {
+    position: absolute;
+    content: '';
+
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: calc(calc(256px / 2) + 30px + 30px);
+
+    background: rgba(0, 0, 0, 0.15);
+
+    z-index: 0;
+  } */
+`;
+
+export const UserHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-bottom: 64px;
+`;
+
+export const UserAvatar = styled.div`
+  width: 256px;
+  height: 256px;
+  position: relative;
+
+  & > img {
+    position: absolute;
+
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+
+    background: ${({ theme }) => theme.bg.tertiary};
+    border-radius: 50%;
+
+    z-index: 2;
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    display: flex;
+
+    top: -4px;
+    right: -4px;
+    width: 264px;
+    height: 264px;
+
+    background: linear-gradient(130.52deg, #FC00FF 35.73%, #00DBDE 70.45%);
+    border-radius: 50%;
+
+    z-index: 1;
+  }
+`;
+
+export const UserName = styled.h1`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-top: 13px;
+
+  color: ${({ theme }) => theme.txt.primary};
+  font-size: 3.5rem;
+  font-family: 'Ubuntu', sans-serif;
+
+  & > span {
+    color: ${({ theme }) => theme.txt.secondary};
+    font-size: 1.1rem;
+    font-weight: normal;
+  }
+`;
+
+export const UserInfos = styled.ul`
+  list-style: none;
+`;
+
+export const UserInfo = styled.li`
+  display: flex;
+  align-items: center;
+
+  padding: 5px;
+  margin: 7px 0;
+
+  border-radius: 4px;
+
+  color: ${({ theme }) => theme.txt.primary};
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 1.2rem;
+
+  transition: 0.2s;
+
+  &:hover {
+    background: ${({ theme }) => theme.bg.secondary};
+  }
+
+  & > svg {
+    margin-right: 5px;
+    
+    color: ${({ theme }) => theme.txt.secondary};
+    font-size: 1.1rem;
+  }
+
+  & > span {
+    display: flex;
+    align-items: center;
+
+    margin-left: auto;
+
+    color: ${({ theme }) => theme.txt.secondary};
+    font-size: 1.4rem;
+    font-family: 'Ubuntu', sans-serif;
+    font-weight: normal;
+  }
+`;
+
+export const LinkButton = styled(Button)`
+  margin: 0 auto;
+  margin-top: 30px;
+`;
+
+export const BackButton = styled.button`
+  position: fixed;
+  display: flex;
+  align-items: center;
+
+  top: 30px;
+  left: 30px;
+  padding: 3px;
+
+  background: none;
+  border: none;
+
+  color: ${({ theme }) => theme.txt.primary};
+  font-size: 1.6rem;
+  font-family: 'Ubuntu', sans-serif;
+
+  cursor: pointer;
+
+  & > svg {
+    margin-right: 3px;
+    color: ${({ theme }) => theme.app.primary}
+  }
 `;
